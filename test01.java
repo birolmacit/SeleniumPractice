782dfe299bf8ead460617df20b4d2a74fe36bc30
Birol Macit <macitbirol@gmail.com>
2022-08-25 00:10:39 +0300

    v1

---
+++ test01.java
@@ -0,0 +1,45 @@
+package myPractices;
+
+import com.beust.jcommander.IStringConverter;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class test01 {
+    /*
+        1.https://www.amazon.com url sine gidin
+        2.baslıgın Amazon  (BUYUK HARFLE) kelimesi icerdiğini test edin
+        3.url nin  https://www.amazon.com   a esit oldugunu test edin
+        4.sayfayı kapatın
+         */
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","src/resources/drivers/chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://www.amazon.com");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize() ;
+
+
+        String actualTitle = driver.getTitle();
+        System.out.println(actualTitle);
+        String aranankelime="Amazon";
+
+        System.out.println(actualTitle.contains(aranankelime) ? "title test passed" : "title test failed");
+
+
+        String actualurl=driver.getCurrentUrl();
+        System.out.println(actualurl);
+        String urlExpected = "https://www.amazon.com";
+
+        System.out.println(actualurl.equals(urlExpected) ? "url test passed" : "url test failed");
+
+        driver.close() ;
+
+
+
+
+    }
+}
---
+++ test02.java
@@ -0,0 +1,59 @@
+package myPractices;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+import java.util.List;
+
+public class test02 {
+    /*Main method oluşturun ve aşağıdaki görevi tamamlayın.
+         a. http://a.testaddressbook.com adresine gidiniz.
+         b. Sign in butonuna basin
+         c. email textbox,password textbox, and signin button elementlerini locate ediniz..
+         d. Kullanıcı adını ve şifreyi aşağıya girin ve oturum aç (sign in)buttonunu tıklayın:
+                 i. Username : testtechproed@gmail.com
+                 ii. Password : Test1234!
+         e. Expected user id nin testtechproed@gmail.com oldugunu dogrulayin(verify).
+         f. “Addresses” ve “Sign Out” textlerinin görüntülendiğini( displayed) doğrulayin(verify).
+
+         Sayfada kac tane link oldugunu bulun.
+
+        */
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver","src/resources/drivers/chromedriver.exe") ;
+        WebDriver tarayıcı=new ChromeDriver() ;
+        tarayıcı.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        tarayıcı.manage().window().maximize();
+
+        tarayıcı.get("http://a.testaddressbook.com");
+        tarayıcı.findElement(By.id("sign-in")).click();
+
+
+
+        WebElement email=tarayıcı.findElement(By.id("session_email"));
+        WebElement password=tarayıcı.findElement(By.id("session_password"));
+        WebElement signin=tarayıcı.findElement(By.name("commit")) ;
+        email.sendKeys("testtechproed@gmail.com");
+        password.sendKeys("Test1234!");
+        signin.click();
+
+
+
+        String expectedemail= tarayıcı.findElement(By.className("navbar-text")).getText();
+        System.out.println(expectedemail);
+        System.out.println(expectedemail.equals("testtechproed@gmail.com") ? "test  passed" : "test failed");
+
+        System.out.println(tarayıcı.findElement(By.className("navbar-text")).isDisplayed() ? "address is displayed" : "address not displayed");
+        System.out.println(tarayıcı.findElement(By.linkText("Sign out")).isDisplayed() ? "signout is displayed" : "signout not displayed");
+
+        List<WebElement > linksayisi=tarayıcı.findElements(By.tagName("a")) ;
+        System.out.println(linksayisi.size());
+
+
+        Thread.sleep(10000);
+        tarayıcı.close();
+    }
+}
